fb32739cfe562cf7323c0af6d9732e3046597654
giangvan767401 <gpham889@gmail.com>
2026-03-11 19:16:43 +0700

    them mau nen

--- lib/man_hinh/man_hinh_dong_ho.dart
+++ lib/man_hinh/man_hinh_dong_ho.dart
@@ -11,12 +11,20 @@ class _ManHinhDongHoState extends State<ManHinhDongHo> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blueGrey[900],
       appBar: AppBar(
         title: const Text('Đồng hồ Pomodoro'),
         centerTitle: true,
+        elevation: 0,
+        backgroundColor: Colors.transparent,
       ),
-      body: const Center(
-        child: Text('Màn hình đồng hồ Pomodoro'),
+      body: SafeArea(
+        child: Center(
+          child: Text(
+            'Màn hình đồng hồ Pomodoro',
+            style: const TextStyle(color: Colors.white, fontSize: 24),
+          ),
+        ),
       ),
     );
   }
