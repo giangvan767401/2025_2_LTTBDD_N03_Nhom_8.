8d7a5a92e2bed04d450353acd79b765d07304036
HuuTinhne <joshuutinh205@gmail.com>
2026-03-14 12:39:23 +0700

    them dem phien

---
+++ lib/models/phien_pomodoro.dart
@@ -0,0 +1,9 @@
+class PhienPomodoro {
+  final DateTime thoiGianHoanThanh;
+  final int thoiLuongPhut; // 25 phút cho focus
+
+  PhienPomodoro({
+    required this.thoiGianHoanThanh,
+    required this.thoiLuongPhut,
+  });
+}
